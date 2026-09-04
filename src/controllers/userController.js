9923ecc8f86943a76b6f0d653968267b58ab6985
Floriano <floriano@email.com>
2026-09-03 21:33:02 -0300

    Atividade aprendendo camadas

---
+++ src/controllers/userController.js
@@ -0,0 +1,13 @@
+import userService from "../service/userService";
+
+const userController = {
+    selecionar: async (req, res) => {
+        try{
+            
+        }catch (error){
+            console.error(error)
+        }
+    }
+}
+
+export default userController;
---
+++ src/service/userService.js
@@ -0,0 +1,26 @@
+import userRepository from "../repositories/userRepository.js";
+
+const userService = {
+    recuperarUsuario:  async () =>{
+        const resultado = await userRepository.selecionar();
+        return resultado;
+    },
+    recuperarUsuarioPorId: async (userId) =>{
+        const resultado = await userRepository.selecionarId(userId);
+        return resultado;
+    },
+    deleteUsuario: async (userId) =>{
+        const resultado = await userRepository.deletarId(userId);
+        return resultado;
+    }, 
+    novoUsuario: async (user) =>{
+        const resultado = await userRepository.criar(user.name, user.email, user.password);
+        return resultado;
+    },
+    atualizarUsuario: async (user) =>{
+        const resultado = await userRepository.atualizar(user.name, user.email, user.password, user.id);
+        return resultado;
+    }
+};
+
+export default userService;
